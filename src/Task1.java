69aa8d11249e9adf362f274a8eaada601b21cd9d
Valeriia Semenskaya <v.semenskaya@ukr.net>
2022-07-28 12:55:23 +0300

    Initial commit of the project

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello. What is your name?");
+        String name = sc.nextLine();
+        System.out.println(name);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Where are you live, "+name+"?");
+        String address = scan.nextLine();
+        System.out.println(address);
+    }
+}
